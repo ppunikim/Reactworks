03ecba5cca691ff195e21edc50bc63baac1265b2
ppunikim <yd62322@naver.com>
2022-07-05 14:44:37 +0900

    a

--- node-100-hello/client/src/comps/BucketInput.jsx
+++ node-100-hello/client/src/comps/BucketInput.jsx
@@ -1,12 +1,51 @@
-const onKeyDown = (e) => {
-  console.log(e.target.value);
-};
+import { useState, useEffect } from "react";
+import BucketList from "./BucketList";
+
 const BucketInput = () => {
+  const [bucketList, setBucketList] = useState([]);
+  const fetchBucketList = async () => {
+    const response = await fetch("http://localhost:3000/bucket/list");
+    const jsonBuck = await response.json();
+    setBucketList(jsonBuck);
+  };
+
+  useEffect(() => {
+    fetchBucketList();
+  }, []);
+
+  const onKeyDown = (e) => {
+    if (e.keyCode === 13) {
+      console.log(e.target.value);
+      const bucket = {
+        b_bucket: e.target.value,
+        b_flag: 0,
+        b_sdate: "2022-07-05 13:20:23",
+        b_cancel: false,
+      };
+
+      //fetch를 사용하여 POST를 이용해, 서버로 데이터 보내기 위한 설정
+      const postData = {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify(bucket),
+      };
+      fetch("http://localhost:3000/bucket", postData)
+        .then((res) => res.text())
+        .then((result) => {
+          if (result === "OK") {
+            fetchBucketList();
+          }
+        });
+    }
+  };
+
   return (
     <>
+      <BucketList bucketList={bucketList} />
       <input onKeyDown={onKeyDown} placeholder="BUCKET" className="w3-input" />
     </>
   );
 };
-
 export default BucketInput;
--- node-100-hello/routes/bucketAPI.js
+++ node-100-hello/routes/bucketAPI.js
@@ -1,11 +1,29 @@
 //NodeJS, Express project의 Web Framework
 import express from "express";
+import { bucket_model } from "../config/mongoConfig.js";
+
 const router = express.Router();
 
 router.get("/", (req, res, next) => {
   const query = req.query.name;
   // res.send("나에게 전달된 값 : " + query);
   res.json({ name: query });
+});
+
+//localhost:3000/bucket주소로 POST 데이터가 전송되어오면,
+router.post("/", (req, res) => {
+  /*
+  bucket_model에 설정된 사항을 참고하여
+  모든 ({}) 데이터를 selectAll(find())하고
+  결과를 result변수에 받고
+  result변수에 받은 데이터를 JSON type으로 client에게 보내라.
+  */
+  const body = req.body;
+  console.table(body);
+  bucket_model.create(body).then((result) => res.send("OK"));
+});
+router.get("/list", (req, res) => {
+  bucket_model.find({}).then((result) => res.json(result));
 });
 
 export default router;
